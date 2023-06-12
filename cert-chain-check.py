f393875498ba36d33d08c2e00a400a59fa7fcabc
rokris <rokris@hotmail.com>
2023-06-12 20:53:08 +0200

    Add more error except output

--- cert-chain-check.py
+++ cert-chain-check.py
@@ -18,7 +18,7 @@ COLOR_RESET = "\033[0m"
 def print_colored(text, color):
     print(color + text + COLOR_RESET)
 
-def check_certificate_chain(server_addresses):
+def check_certificate_chain(server_addresses, server_port):
     try:
         # Create an SSL context and connect to each server
         context = ssl.create_default_context(cafile=certifi.where())
@@ -26,7 +26,7 @@ def check_certificate_chain(server_addresses):
         for server_address in server_addresses:
             try:
                 with context.wrap_socket(socket.socket(), server_hostname=server_address) as s:
-                    s.connect((server_address, 443))
+                    s.connect((server_address, server_port))
                     cert_der = ssl.DER_cert_to_PEM_cert(s.getpeercert(binary_form=True))
 
                     # Validate the certificate chain
@@ -49,27 +49,37 @@ def check_certificate_chain(server_addresses):
                 except ExtensionNotFound:
                     raise ssl.CertificateError("Certificate does not contain Subject Alternative Name (SAN) extension")
 
-                print_colored(f"Certificate and chain are valid for {server_address}.", COLOR_GREEN)
+                print_colored(f"Certificate and chain are valid for {server_address}", COLOR_GREEN)
 
             except ssl.SSLError as e:
                 error_message = str(e)
                 if "unable to get local issuer certificate" in error_message:
-                    print_colored(f"Certificate or chain validation failed for {server_address}: The local issuer certificate is not available.", COLOR_RED)
+                    print_colored(f"Certificate or chain validation failed for {server_address}: The local issuer certificate is not available", COLOR_RED)
                 else:
                     print_colored(f"Certificate or chain validation failed for {server_address}: {error_message}", COLOR_RED)
 
             except ssl.CertificateError as e:
                 error_message = str(e)
-                print_colored(f"Hostname validation failed for {server_address}: {error_message}", COLOR_RED)
+                print_colored(f"Hostname validation failed for {server_address}", COLOR_RED)
+
+            except ConnectionResetError as e:
+                print_colored(f"Connection reset by peer for {server_address}", COLOR_RED)
+
+            except ConnectionRefusedError as e:
+                print_colored(f"Connection refused by peer for {server_address}", COLOR_RED)
 
     except socket.gaierror:
-        print_colored("Invalid server address or hostname not known.", COLOR_RED)
+        print_colored("Invalid server address or hostname not known", COLOR_RED)
 
 if __name__ == '__main__':
     # Get the server addresses from command-line arguments
     server_addresses = sys.argv[1:]
+    server_port = 443
     # Check if no command-line arguments are provided
     if not server_addresses:
         # Prompt for server addresses if none are provided
-        server_addresses = input("Enter the server addresses (separated by spaces): ").split()
-    check_certificate_chain(server_addresses)
+        server_addresses = input("Enter the server addresses (separated multiple hosts with spaces): ").split()
+        server_port = input("Enter the server port (default port is 443): ")
+        if server_port : server_port = int(server_port)
+        else : server_port = 443
+    check_certificate_chain(server_addresses, server_port)
