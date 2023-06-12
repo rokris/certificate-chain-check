a01ebc8ccdcc7781a197be438b4533e8daf2a449
rokris <rokris@hotmail.com>
2023-06-12 21:05:36 +0200

    Add doc string

--- cert-chain-check.py
+++ cert-chain-check.py
@@ -15,39 +15,64 @@ COLOR_RED = "\033[91m"
 COLOR_GREEN = "\033[92m"
 COLOR_RESET = "\033[0m"
 
+
 def print_colored(text, color):
+    """
+    Prints the given text in the specified color.
+    """
     print(color + text + COLOR_RESET)
 
+
+def validate_certificate_chain(context, server_address, server_port):
+    """
+    Validates the certificate chain for the given server address and port.
+    Returns the loaded certificate object if successful.
+    Raises SSL errors if certificate validation fails.
+    """
+    with context.wrap_socket(socket.socket(), server_hostname=server_address) as s:
+        s.connect((server_address, server_port))
+        cert_der = ssl.DER_cert_to_PEM_cert(s.getpeercert(binary_form=True))
+
+        # Validate the certificate chain
+        cert_store = context.get_ca_certs()
+        cert = load_pem_x509_certificate(cert_der.encode(), default_backend())
+        cert_store.append(cert)
+        context.verify_mode = ssl.CERT_REQUIRED
+        context.check_hostname = False  # Disable default hostname validation
+        context.cert_store = cert_store
+
+    return cert
+
+
+def validate_hostname(cert, server_address):
+    """
+    Validates the hostname of the certificate against the server address.
+    Raises SSL errors if hostname validation fails.
+    """
+    try:
+        san_extension = cert.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
+        cert_alt_names = san_extension.value.get_values_for_type(DNSName)
+        for alt_name in cert_alt_names:
+            if fnmatch.fnmatch(server_address, alt_name):
+                return True
+        raise ssl.CertificateError(f"Hostname mismatch: expected {server_address}, got {cert_alt_names}")
+    except ExtensionNotFound:
+        raise ssl.CertificateError("Certificate does not contain Subject Alternative Name (SAN) extension")
+
+
 def check_certificate_chain(server_addresses, server_port):
+    """
+    Checks the certificate chain for each server address and port combination.
+    Prints the validation results for each server.
+    """
     try:
         # Create an SSL context and connect to each server
         context = ssl.create_default_context(cafile=certifi.where())
 
         for server_address in server_addresses:
             try:
-                with context.wrap_socket(socket.socket(), server_hostname=server_address) as s:
-                    s.connect((server_address, server_port))
-                    cert_der = ssl.DER_cert_to_PEM_cert(s.getpeercert(binary_form=True))
-
-                    # Validate the certificate chain
-                    cert_store = context.get_ca_certs()
-                    cert = load_pem_x509_certificate(cert_der.encode(), default_backend())
-                    cert_store.append(cert)
-                    context.verify_mode = ssl.CERT_REQUIRED
-                    context.check_hostname = False  # Disable default hostname validation
-                    context.cert_store = cert_store
-
-                # Validate the hostname against Subject Alternative Names (SAN)
-                try:
-                    san_extension = cert.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME)
-                    cert_alt_names = san_extension.value.get_values_for_type(DNSName)
-                    for alt_name in cert_alt_names:
-                        if fnmatch.fnmatch(server_address, alt_name):
-                            break
-                    else:
-                        raise ssl.CertificateError(f"Hostname mismatch: expected {server_address}, got {cert_alt_names}")
-                except ExtensionNotFound:
-                    raise ssl.CertificateError("Certificate does not contain Subject Alternative Name (SAN) extension")
+                cert = validate_certificate_chain(context, server_address, server_port)
+                validate_hostname(cert, server_address)
 
                 print_colored(f"Certificate and chain are valid for {server_address}", COLOR_GREEN)
 
@@ -62,14 +87,15 @@ def check_certificate_chain(server_addresses, server_port):
                 error_message = str(e)
                 print_colored(f"Hostname validation failed for {server_address}", COLOR_RED)
 
-            except ConnectionResetError as e:
+            except ConnectionResetError:
                 print_colored(f"Connection reset by peer for {server_address}", COLOR_RED)
 
-            except ConnectionRefusedError as e:
+            except ConnectionRefusedError:
                 print_colored(f"Connection refused by peer for {server_address}", COLOR_RED)
 
     except socket.gaierror:
         print_colored("Invalid server address or hostname not known", COLOR_RED)
+
 
 if __name__ == '__main__':
     # Get the server addresses from command-line arguments
@@ -80,6 +106,8 @@ if __name__ == '__main__':
         # Prompt for server addresses if none are provided
         server_addresses = input("Enter the server addresses (separated multiple hosts with spaces): ").split()
         server_port = input("Enter the server port (default port is 443): ")
-        if server_port : server_port = int(server_port)
-        else : server_port = 443
+        if server_port: 
+            server_port = int(server_port)
+        else:
+            server_port = 443
     check_certificate_chain(server_addresses, server_port)
